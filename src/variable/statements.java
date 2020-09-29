67e773f7d766f70084c038d5d9f5bbee29328d38
given414 <given414@gmail.com>
2020-09-29 11:40:14 +0900

    first

---
+++ src/variable/statements.java
@@ -0,0 +1,17 @@
+package variable;
+
+public class statements {
+	public static void main(String[] args) {
+		System.out.println("첫번째 명령문");
+		System.out.println("두번째 명령문");
+		System.out.println("세번째 명령문");
+		
+		// 한 줄 주석
+		/*
+		 * 
+		 * 여러 줄 주석
+		 * 여러 줄 주석
+		 *
+		 */
+	}
+}
