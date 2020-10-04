1a3e108c3df69668dfd6ea4a40e6de2c904ff754
given414 <given414@gmail.com>
2020-10-04 20:11:51 +0900

    home

--- src/chap02/textbook/BooleanExample.java
+++ src/chap02/textbook/BooleanExample.java
@@ -2,7 +2,7 @@ package chap02.textbook;
 
 public class BooleanExample {
 	public static void main(String[] args) {
-		boolean stop = true;
+		boolean stop = false;
 		if (stop) {
 			System.out.println("중지합니다.");
 		} else {
