56627209eee3d8e4a60a9092f43877055d9f8128
given414 <given414@gmail.com>
2020-10-05 17:42:22 +0900

    exercise

---
+++ src/chap03/textbook/Exercise02.java
@@ -0,0 +1,11 @@
+package chap03.textbook;
+
+public class Exercise02 {
+	public static void main(String[] args) {
+		int x = 10;
+		int y = 20;
+		int z = (++x) + (y--); // 증감 연산자가 변수 뒤에 있으면 다른 연산자를 먼저 처리한 후 1증가 또는 1감소시킨다
+		System.out.println(z);
+	}
+
+}
