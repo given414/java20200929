c2e25ba542009511f73d913b990f8eb6f72d4359
given414 <given414@gmail.com>
2020-10-05 16:42:55 +0900

    chap03

---
+++ src/chap03/binary/ArithmeticOperator.java
@@ -0,0 +1,19 @@
+package chap03.binary;
+
+public class ArithmeticOperator {
+	public static void main(String[] args) {
+		// + - *
+		int v1 = 5;
+		int v2 = 2;
+		
+		int result1 = v1 + v2;
+		System.out.println("result1=" + result1);
+		
+		int result2 = v1 - v2;
+		System.out.println("result2=" + result2);
+		
+		int result3 = v1 * v2;
+		System.out.println("result3=" + result3);
+	}
+
+}
