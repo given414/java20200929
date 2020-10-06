2238bf9760ca5e464eb73c1bc6ff4304d300faac
given414 <given414@gmail.com>
2020-10-06 16:50:45 +0900

    CodingBAt Warmup

--- src/codingbat/WarmUp1.java
+++ src/codingbat/WarmUp1.java
@@ -1,6 +1,7 @@
 package codingbat;
 
 public class WarmUp1 {
+	
 	public boolean sleepIn(boolean weekday, boolean vacation) {
 		
 		return !weekday || vacation;
@@ -30,5 +31,43 @@ public class WarmUp1 {
 		return talking && ( hour < 7 || hour >20);
 	}
 	
-
+	public boolean makes10(int a, int b) {
+		int sum = a + b;
+		
+		return (sum == 10) || (a == 10) || (b == 10);		
+	}
+	
+	public boolean nearHundred(int n)  {
+		boolean d100 = (n >= 90) && (n <= 110);
+		boolean d200 = (n >= 190) && (n <= 210);
+		
+		return d100 || d200;
+	}
+	
+	public boolean posNeg(int a, int b, boolean negative) {
+		boolean aneg = a < 0;
+		boolean bneg = b < 0;
+		
+		return negative ? (aneg && bneg) : (aneg ^ bneg);
+	}
+	
+	
+	public String notString(String str) {
+		int ind = str.indexOf("not");
+		
+		if (ind !=0) {
+			String res = "not" + str;
+			return res;
+		}
+		
+		return str;
+	}
+	
+	public String missingChar (String str, int n) {
+		String left = str.substring(0, n);
+		String right = str.substring(n + 1);
+		
+		return left + right;
+	}
+	
 }
