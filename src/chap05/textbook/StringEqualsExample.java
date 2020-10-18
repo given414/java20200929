17247a84e274528bb0594bc3b05d10d2751424a5
given414 <given414@gmail.com>
2020-10-19 00:01:30 +0900

    re

--- src/chap05/textbook/StringEqualsExample.java
+++ src/chap05/textbook/StringEqualsExample.java
@@ -7,11 +7,11 @@ public class StringEqualsExample {
 		
 		if(strVar1 == strVar2) {
 			System.out.println("strVar1과 strVar2는 참조가 같음");
-		} else {
-			System.out.println("strVar1곤 strVar2는 참조가 다름");
+		}  else {
+			System.out.println("strVar1과 strVar2는 참조가 다름");
 		}
 		
-		if(strVar1.contentEquals(strVar2)) {
+		if(strVar1.equals(strVar2)) {
 			System.out.println("strVar1과 strVar2는 문자열이 같음");
 		}
 		
