778bfaaaee1c8eb04d9b7f179d953913af0d63d2
given414 <given414@gmail.com>
2020-10-25 23:18:42 +0900

    re

--- src/chap06/textbook/s060801/CalculatorExample.java
+++ src/chap06/textbook/s060801/CalculatorExample.java
@@ -6,14 +6,14 @@ public class CalculatorExample {
 		myCalc.powerOn();
 		
 		int result1 = myCalc.plus(5, 6);
-		System.out.println("result1: " + result1);
+		System.out.println("resul1: " + result1);
+		
 		
 		byte x = 10; //바이트 타입은 int 타입으로 자동 형변환 가능
 		byte y = 4;
-		double result2 = myCalc.divide(x,y);
-		System.out.println("result2: " +result2);
-		myCalc.powerOff();
+		double result2 = myCalc.divide(x, y);
+		System.out.println("result2: " + result2);
 		
+		myCalc.powerOff();
 	}
-
 }
